c87246920f9958e9c22aeb781828500798378fc6
sergi.casas.monserrate <sergi.casas.monserrate@est.fib.upc.edu>
2018-03-30 17:30:32 +0200

    activity login

--- app/src/main/java/com/example/usuario/rekindlefrontend/MainActivity.java
+++ app/src/main/java/com/example/usuario/rekindlefrontend/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.usuario.rekindlefrontend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button b = (Button) findViewById(R.id.login);
+        Spinner spinner = (Spinner) findViewById(R.id.lista_usuarios);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lista_usuarios, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         b.setOnClickListener(new View.OnClickListener(){
 
