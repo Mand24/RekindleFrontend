d460a79801dafc0a2db5dfe07edea94d82d5a5f8
sergi.casas.monserrate <sergi.casas.monserrate@est.fib.upc.edu>
2018-03-31 17:22:13 +0200

    fragment eleccion registro funcionando

--- app/src/main/java/com/example/usuario/rekindlefrontend/RegistroUsuario.java
+++ app/src/main/java/com/example/usuario/rekindlefrontend/RegistroUsuario.java
@@ -1,13 +1,33 @@
 package com.example.usuario.rekindlefrontend;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegistroUsuario extends AppCompatActivity {
 
+    Fragment [] tiposRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        tiposRegistro = new Fragment[2];
+
+        tiposRegistro[0] = new RegistroRefugiado();
+
+        tiposRegistro[1] = new RegistroVoluntario();
+
+        menu(0);
+    }
+
+    public void menu(int tipo){
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.formulario, tiposRegistro[tipo]);
+        transaction.commit();
     }
 }
