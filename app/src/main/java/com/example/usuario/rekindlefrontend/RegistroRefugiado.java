ab7ff866be41001c29958fc2bfd000d0177fa612
sergi.casas.monserrate <sergi.casas.monserrate@est.fib.upc.edu>
2018-04-03 16:28:05 +0200

    formularios arreglados

--- app/src/main/java/com/example/usuario/rekindlefrontend/RegistroRefugiado.java
+++ app/src/main/java/com/example/usuario/rekindlefrontend/RegistroRefugiado.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -23,7 +25,39 @@ public class RegistroRefugiado extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_refugiado, container, false);
+        View view = inflater.inflate(R.layout.fragment_registro_refugiado, container, false);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.sexo_refugiado);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity()
+                .getApplicationContext(), R.array.lista_sexo, R.layout.spinner_item);
+
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner = (Spinner) view.findViewById(R.id.grupo_sanguineo_refugiado);
+
+        adapter = ArrayAdapter.createFromResource(getActivity()
+                .getApplicationContext(), R.array.lista_grupo_sanguineo, R.layout.spinner_item);
+
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner = (Spinner) view.findViewById(R.id.ojos_refugiado);
+
+        adapter = ArrayAdapter.createFromResource(getActivity()
+                .getApplicationContext(), R.array.lista_color_ojos, R.layout.spinner_item);
+
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        return view;
     }
 
 }
