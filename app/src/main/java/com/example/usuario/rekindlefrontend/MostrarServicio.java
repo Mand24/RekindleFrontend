8d8a0dafe98ca9e3626723dd9b133a6f16f21d81
Raul <raul.gavara@gmail.com>
2018-04-10 17:29:59 +0200

    Google Maps

--- app/src/main/java/com/example/usuario/rekindlefrontend/MostrarServicio.java
+++ app/src/main/java/com/example/usuario/rekindlefrontend/MostrarServicio.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MostrarServicio extends AppCompatActivity {
+public class MostrarServicio extends AppCompatActivity implements OnMapReadyCallback {
 
     TextView titulo, descripcion, direccion, fecha, numero, valoracion;
-    MapView googleMaps;
+    SupportMapFragment mMapView;
+    GoogleMap mGoogleMap;
     AppCompatButton chat, opiniones, inscribirse;
 
     @Override
@@ -22,7 +28,8 @@ public class MostrarServicio extends AppCompatActivity {
         descripcion = (TextView) findViewById(R.id.descripcion_servicio);
         direccion = (TextView) findViewById(R.id.direccion_servicio);
         fecha = (TextView) findViewById(R.id.fecha_limite_servicio);
-        googleMaps = (MapView) findViewById(R.id.google_mapView);
+        mMapView = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_mapView);
         numero = (TextView) findViewById(R.id.numero_contacto_servicio);
         chat = (AppCompatButton) findViewById(R.id.chat);
         valoracion = (TextView) findViewById(R.id.valoracion_servicio);
@@ -37,5 +44,14 @@ public class MostrarServicio extends AppCompatActivity {
         fecha.setText(servicio.getFecha());
         numero.setText(servicio.getNumero());
         valoracion.setText("Valoracion: " + servicio.getValoracion());
+
+        mMapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
