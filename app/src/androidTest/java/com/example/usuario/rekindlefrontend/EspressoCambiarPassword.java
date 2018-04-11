18e5406a64a0860ae5f8b4a99f9ebf0ecc01b89a
Manel <manel.fernandez@est.fib.upc.edu>
2018-04-11 15:41:35 +0200

    tests

---
+++ app/src/androidTest/java/com/example/usuario/rekindlefrontend/EspressoCambiarPassword.java
@@ -0,0 +1,109 @@
+package com.example.usuario.rekindlefrontend;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.content.Intent;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.Menu;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.init;
+import static android.support.test.espresso.intent.Intents.release;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.times;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+//import static org.hamcrest.core.AllOf.allOf;
+//import static org.hamcrest.core.Is.is;
+//import static org.hamcrest.core.IsInstanceOf.instanceOf;
+//import static org.hamcrest.core.AnyOf.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
+
+
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoCambiarPassword {
+
+    @Rule
+    public ActivityTestRule<CambiarPassword> pantalla = new ActivityTestRule<CambiarPassword>
+            (CambiarPassword.class);
+
+    @BeforeClass
+    public static void setup(){
+        init();
+    }
+
+    @AfterClass
+    public static void end(){
+        release();
+    }
+
+    @Test
+    public void testCamposContraseña() {
+
+        onView(withId(R.id.actual_password)).perform(replaceText
+                ("testPasswordActual"), ViewActions.closeSoftKeyboard
+                ()).check(matches(withText("testPasswordActual")));
+
+        onView(withId(R.id.new_password)).perform(replaceText
+                ("testPasswordNuevo"), ViewActions.closeSoftKeyboard
+                ()).check(matches(withText("testPasswordNuevo")));
+
+        onView(withId(R.id.repeat_password)).perform(replaceText
+                ("testPasswordNuevo"), ViewActions.closeSoftKeyboard
+                ()).check(matches(withText("testPasswordNuevo")));
+
+
+    }
+
+    @Test
+    public void testBotonGuardar() {
+
+        //TODO: Testear cuando funcione cambiar password
+
+    }
+
+    @Test
+    public void testAtras(){
+
+        //TODO: Testear cuando tengamos boton atras
+
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/usuario/rekindlefrontend/SuiteEspresso.java
@@ -0,0 +1,15 @@
+package com.example.usuario.rekindlefrontend;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by ORION on 11/04/2018.
+ */
+
+// Runs all unit tests.
+@RunWith(Suite.class)
+@Suite.SuiteClasses({EspressoMenuPrincipal.class, EspressoPantallaInicio.class,
+        EspressoRegistroUsuario.class,EspressoVerPerfil.class, EspressoCambiarPassword.class,
+        EspressoEditarPerfil.class})
+public class SuiteEspresso {}
