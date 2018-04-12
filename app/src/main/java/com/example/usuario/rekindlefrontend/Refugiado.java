2e4a555c266001bca6c675996c05147a86a14d13
Raul <raul.gavara@gmail.com>
2018-04-12 19:33:47 +0200

    Creacion clases usuario

---
+++ app/src/main/java/com/example/usuario/rekindlefrontend/Refugiado.java
@@ -0,0 +1,85 @@
+package com.example.usuario.rekindlefrontend;
+
+public class Refugiado extends Usuario {
+
+    private String phoneNumber, birthDate, sex, country, town, ethnic, bloodType, eyeColor;
+
+    public Refugiado(String mail, String password, String name, String surname1,
+            String surname2, String phoneNumber, String birthDate, String sex, String country,
+            String town, String ethnic, String bloodType, String eyeColor) {
+        super(mail, password, name, surname1, surname2);
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.sex = sex;
+        this.country = country;
+        this.town = town;
+        this.ethnic = ethnic;
+        this.bloodType = bloodType;
+        this.eyeColor = eyeColor;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getEthnic() {
+        return ethnic;
+    }
+
+    public void setEthnic(String ethnic) {
+        this.ethnic = ethnic;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+}
---
+++ app/src/main/java/com/example/usuario/rekindlefrontend/Voluntario.java
@@ -0,0 +1,9 @@
+package com.example.usuario.rekindlefrontend;
+
+public class Voluntario extends Usuario {
+
+    public Voluntario(String mail, String password, String name, String surname1,
+            String surname2) {
+        super(mail, password, name, surname1, surname2);
+    }
+}
