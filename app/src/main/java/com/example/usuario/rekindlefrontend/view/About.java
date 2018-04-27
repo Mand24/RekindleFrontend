5655e4ece1338bd071577f361005572c1ea99dc3
manel.fernandez <manel.fernandez@est.fib.upc.edu>
2018-04-27 10:00:00 +0200

    about llest per ser cridat

--- app/src/main/java/com/example/usuario/rekindlefrontend/view/About.java
+++ app/src/main/java/com/example/usuario/rekindlefrontend/view/About.java
@@ -1,6 +1,11 @@
 package com.example.usuario.rekindlefrontend.view;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.example.usuario.rekindlefrontend.AppBaseActivity;
+import com.example.usuario.rekindlefrontend.R;
+import com.example.usuario.rekindlefrontend.view.menu.MenuPrincipal;
 
 /**
  * Created by Manel Fernandez on 27-Apr-18.
@@ -8,5 +13,16 @@ import com.example.usuario.rekindlefrontend.AppBaseActivity;
 
 public class About extends AppBaseActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getApplicationContext(), MenuPrincipal.class);
+        startActivity(i);
+    }
 
 }
