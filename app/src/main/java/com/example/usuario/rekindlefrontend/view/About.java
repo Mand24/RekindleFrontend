0e78af607e4fceb7b022e2e53b3021b5e9c82d76
manel.fernandez <manel.fernandez@est.fib.upc.edu>
2018-04-27 09:04:09 +0200

    pantalla about

---
+++ app/src/main/java/com/example/usuario/rekindlefrontend/view/About.java
@@ -0,0 +1,12 @@
+package com.example.usuario.rekindlefrontend.view;
+
+import com.example.usuario.rekindlefrontend.AppBaseActivity;
+
+/**
+ * Created by Manel Fernandez on 27-Apr-18.
+ */
+
+public class About extends AppBaseActivity {
+
+
+}
