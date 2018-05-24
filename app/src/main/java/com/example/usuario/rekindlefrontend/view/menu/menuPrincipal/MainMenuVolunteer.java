998e8e1b2320d4da714411aea6b72957f05cf843
sergi.casas.monserrate <sergi.casas.monserrate@est.fib.upc.edu>
2018-05-24 21:09:13 +0200

    boto chat menu voluntari

--- app/src/main/java/com/example/usuario/rekindlefrontend/view/menu/menuPrincipal/MainMenuVolunteer.java
+++ app/src/main/java/com/example/usuario/rekindlefrontend/view/menu/menuPrincipal/MainMenuVolunteer.java
@@ -9,10 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.example.user.rekindlefrontend.R;
 import com.example.usuario.rekindlefrontend.view.services.create.CreateService;
 import com.example.usuario.rekindlefrontend.view.services.list.ListServices;
 import com.example.usuario.rekindlefrontend.view.services.list.MyServicesVolunteer;
+
+import com.example.usuario.rekindlefrontend.R;
+import com.example.usuario.rekindlefrontend.view.servicios.crear.CrearServicio;
+import com.example.usuario.rekindlefrontend.view.servicios.listar.ListarServicios;
+import com.example.usuario.rekindlefrontend.view.servicios.listar.MisServiciosVoluntario;
+import com.example.usuario.rekindlefrontend.view.usuarios.chat.ListChats;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +30,7 @@ public class MainMenuVolunteer extends Fragment {
     private AppCompatButton button_list_services;
     private AppCompatButton button_create_services;
     private AppCompatButton button_my_services;
-
+    private AppCompatButton button_chat;
 
     public MainMenuVolunteer() {
         // Required empty public constructor
@@ -65,14 +73,25 @@ public class MainMenuVolunteer extends Fragment {
             }
         });
 
+        button_chat.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity().getApplicationContext(), ListChats.class);
+                startActivity(i);
+            }
+        });
+
 
         return view;
     }
-
+    
     public void setViews(View view){
         button_list_services = view.findViewById(R.id.listar_servicios_MenuPrincipalVoluntario);
         button_create_services = view.findViewById(R.id.crear_servicio_MenuPrincipalVoluntario);
         button_my_services = view.findViewById(R.id.mis_servicios_MenuPrincipalVoluntario);
+        button_chat = view.findViewById(R.id.chat);
+
     }
 
 }
