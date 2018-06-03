5e0bb476e857aa4d429ab51120440c708c372440
= <=>
2018-06-03 18:13:29 +0200

    pantalla valorar servicio

---
+++ app/src/main/java/com/example/usuario/rekindlefrontend/view/services/EvaluateService.java
@@ -0,0 +1,82 @@
+package com.example.usuario.rekindlefrontend.view.services;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.usuario.rekindlefrontend.R;
+import com.example.usuario.rekindlefrontend.data.entity.service.Lodge;
+import com.example.usuario.rekindlefrontend.data.entity.service.Service;
+import com.example.usuario.rekindlefrontend.data.entity.user.Refugee;
+
+public class EvaluateService extends AppCompatActivity {
+
+    private Service serviceEx = new Lodge (1, "voluntario@gmail.com", "alojamiento", "desc",
+            "Carrer Infern d'en Parera, 2, 08348 Cabrils, Barcelona", "12","12-03-2018", "1234");
+
+    Refugee refugeeEx = new Refugee ("refugiado@gmail.com", "pass1234", "refugiadoName",
+            "refugiadoSurname","refugadioSecondSurname", "foto", "123456789","12-09-2018","Male",
+            "Barcelona","cabrils","ethinc","+A","","");
+
+    private TextView  serviceName;
+    private RatingBar ratingBar;
+    private EditText  serviceOpinion;
+
+    private Refugee refugee;
+    private Service service;
+    private Float   rating;
+    private String  opinion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluate_service);
+
+        refugee = refugeeEx;
+        service = serviceEx;
+
+        setViews ();
+
+        AppCompatButton button_send = (AppCompatButton) findViewById(R.id
+                .evaluateService);
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    checkOpinion ();
+                    rating  = ratingBar.getRating();
+
+                    System.out.println("Rating bar: " + rating);
+                    System.out.println("Opinion: " + opinion.length());
+                } catch (Exception e) {
+                    Toast.makeText(v.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void setViews () {
+        serviceName     = findViewById (R.id.serviceName);
+        ratingBar       = findViewById (R.id.ratingBar);
+        serviceOpinion  = findViewById (R.id.service_opinion);
+
+        serviceName.setText (service.getName());
+    }
+
+    private void checkOpinion () {
+
+        opinion = serviceOpinion.getText().toString();
+
+        if (opinion.length() > 350) {
+            Toast.makeText(getApplicationContext(), getText(R.string.opinionLenght), Toast
+                    .LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+}
